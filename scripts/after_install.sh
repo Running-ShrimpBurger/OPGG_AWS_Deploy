daf603eff32a28439cff3df4583710a001b1e8fb
JuneBin-Eom <ejb0329@gmail.com>
2021-04-18 23:43:32 +0900

    add yarn pm

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/server
-sudo npm install
-npm audit fix
+sudo yarn
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,3 +2,8 @@
 cd /home/ec2-user/server
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install nodejs npm -y
+
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo rpm --import https://dl.yarnpkg.com/rpm/pubkey.gpg
+
+sudo yum install yarn -y
