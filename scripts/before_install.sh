a09fafe50bbd7aa9caa516bf7b59b76b1e8f2ef4
JuneBin-Eom <ejb0329@gmail.com>
2021-04-19 00:03:15 +0900

    fix scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,11 @@
 cd /home/ec2-user/server
 
 sudo rm .env
+sudo rm -rf node_modules/
+
+sudo touch .env
+sudo chmod 777 .env
+echo "SKIP_PREFLIGHT_CHECK=true" > .env
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install nodejs npm -y
