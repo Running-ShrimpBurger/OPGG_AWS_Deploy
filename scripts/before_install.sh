638e62c61e969e427efe69b0e11cfd165ff23438
JuneBin-Eom <ejb0329@gmail.com>
2021-04-19 21:59:21 +0900

    change buildspec.yml

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,7 @@ sudo rm -rf node_modules/
 
 sudo touch .env
 sudo chmod 777 .env
-echo "SKIP_PREFLIGHT_CHECK=true" > .env
+sudo echo "SKIP_PREFLIGHT_CHECK=true" > .env
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install nodejs npm -y
